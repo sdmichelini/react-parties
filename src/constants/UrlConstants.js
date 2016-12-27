3b138ba9e0e1bdb0c513a2ac0a98ae5d9f40c290
Stephen Michelini <sdmichelini@wpi.edu>
2016-12-26 23:26:44 -0500

    Updated Dist

--- src/constants/UrlConstants.js
+++ src/constants/UrlConstants.js
@@ -1,2 +1,4 @@
-export const URL = 'http://localhost:3001';
-export const WS_URL = 'ws://localhost:3001';
+import config from 'config';
+
+export const URL = (config.appEnv == 'dev')?'http://localhost:3001':'https://js-list.herokuapp.com';
+export const WS_URL = (config.appEnv == 'dev')?'ws://localhost:3001':'wss://js-list.herokuapp.com;'
