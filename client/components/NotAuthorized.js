cd596108c76d1069fa536946f5f0acbdd98dece8
S M <S M>
2017-01-22 14:01:22 -0500

    Added Help Message

--- client/components/NotAuthorized.js
+++ client/components/NotAuthorized.js
@@ -7,7 +7,10 @@ class NotAuthroizedComponent extends Component {
   }
   render() {
     return (
-      <h2>Not authorized to view the resource.</h2>
+      <div>
+        <h2>Not authorized to view the resource.</h2>
+        <p className='text-muted'>If social has just approved you, log out and log back in.</p>
+      </div>
     );
   }
 }
